3458e810f888f86e6ab8c7e16bf2aabe875cd11a
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-18 21:09:44 -0800

    stubbed in

---
+++ ActionUnload/ActionUnload.h
@@ -0,0 +1,13 @@
+//
+//  ActionUnload.h
+//  ActionUnload
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <RSTrixiePlugin/RSTrixiePlugin.h>
+
+@interface ActionUnload : RSTrixiePlugin
+
+@end
---
+++ ActionUnload/ActionUnload.m
@@ -0,0 +1,26 @@
+//
+//  ActionUnload.m
+//  ActionUnload
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "ActionUnload.h"
+
+@implementation ActionUnload
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	
+    if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
+    {
+		[self setName: @"Unload"];
+    }
+    return self;
+}
+
+- (BOOL) hasSelectorField {
+	return NO;
+}
+
+@end
